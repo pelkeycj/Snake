8bd9518227c14448b773ac3ff73f1cef206551a9
Connor Pelkey <Pelkey.c@husky.neu.edu>
2016-12-09 02:41:05 -0500

    fixes drawing error

--- game.py
+++ game.py
@@ -1,10 +1,11 @@
 import pygame
 from snake import *
 
+#colors
+white = (255, 255, 255)
+black = (0, 0, 0)
+
 def main():
-    #colors
-    white = (255, 255, 255)
-    black = (0, 0, 0)
     #initializations
     pygame.init()
     WIDTH = 800
@@ -13,19 +14,36 @@ def main():
     pygame.display.set_caption('Snake')
     gameDisplay.fill(white)
     pygame.display.update()
+    clock = pygame.time.Clock()
     snake = Snake(WIDTH/2, HEIGHT/2) #initialize player
 
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT: #exit game
-                pygame.quit()
-            if event.type == pygame.KEYDOWN: # control direction
-                if event.key == pygame.K_LEFT or \
-                        event.key == pygame.K_RIGHT or \
-                        event.key == pygame.K_UP or \
-                        event.key == pygame.K_DOWN:
-                    snake.changeDirection(event.key)
+        clock.tick(60) #clock runs at 5 FPS
 
+        handleEvents(snake)                      # read inputs
+        snake.move()                        # update snake position
+        gameDisplay.fill(white)             # clear screen
+        render(gameDisplay, snake)          # draw snake
+        pygame.display.update()
+
+def handleEvents(snake):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: #exit game
+            pygame.quit()
+        if event.type == pygame.KEYDOWN: # control direction
+            print(event.type)
+            if event.key == pygame.K_LEFT:
+                snake.changeDirection("left")
+            elif event.key == pygame.K_RIGHT:
+                snake.changeDirection("right")
+            elif event.key == pygame.K_UP:
+                snake.changeDirection("up")
+            elif event.key == pygame.K_DOWN:
+                snake.changeDirection("down")
+
+def render(gameDisplay, snake):
+    for segment in snake.segments:
+        pygame.draw.circle(gameDisplay, black, (segment.x, segment.y), snake.radius, 0)
 
 
 if __name__ == "__main__":
