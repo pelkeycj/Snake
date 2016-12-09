960b78c8071478948585170d1e0b0ec6ec9c3fb9
Connor Pelkey <Pelkey.c@husky.neu.edu>
2016-12-09 01:23:17 -0500

    direction control

--- game.py
+++ game.py
@@ -1,21 +1,34 @@
 import pygame
+import snake
 
 def main():
     #colors
-    white(255, 255, 255)
+    white = (255, 255, 255)
+    black = (0, 0, 0)
     #initializations
     pygame.init()
-    gameDisplay = pygame.display.set_mode((800, 600))
+    WIDTH = 800
+    HEIGHT = 600
+    gameDisplay = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption('Snake')
     gameDisplay.fill(white)
     pygame.display.update()
+    snake = Snake() #initialize player
 
     while True:
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT: #exit game
                 pygame.quit()
+            if event.type == pygame.KEYDOWN: # control direction
+                if event.key == pygame.K_LEFT or \
+                        event.key == pygame.K_RIGHT or \
+                        event.key == pygame.K_UP or \
+                        event.key == pygame.K_DOWN:
+                    snake.changeDirection(event.key)
 
-        gameDisplay.update()
+        gameDisplay.fill(white)
+        pygame.draw.rect(gameDisplay, black, [WIDTH/2, HEIGHT/2, 10, 50])
+        pygame.display.update()
 
 if __name__ == "__main__":
     main()
